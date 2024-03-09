4e3d21a6b595244e3bbcff579041866ceed6d9dd
Daniel Heras Quesada <dani.heras@hotmail.com>
2024-03-09 10:39:42 +0100

    rate limit to protect from brute-force attacks

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Throttle } from '@nestjs/throttler';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // Override default configuration for Rate limiting and duration.
+  @Throttle({ default: { limit: 10, ttl: 1000 } })
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 10000,
+        limit: 20,
+      },
+    ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
